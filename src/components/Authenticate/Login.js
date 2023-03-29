e5e8c8ca53bdfc22b7c45ad28ad07247b9343fc0
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-29 15:45:30 +0300

    added the input fileds

--- src/components/Authenticate/Login.js
+++ src/components/Authenticate/Login.js
@@ -14,8 +14,16 @@ const GoHome = () => {
     return(
         <div>
             <h1>Login</h1>
-            <button onClick={OpenSignup}>Create-Account</button>
+            <div className="txt_field">
+                <label htmlFor="email">Email: </label>
+                <input type="email" name="" id="" />
+            </div>
+            <div className="txt_field">
+                <label htmlFor="password">Password: </label>
+                <input type="email" name="" id="" />
+            </div>
             <button onClick={GoHome}>Login</button>
+            <button onClick={OpenSignup}>Create-Account</button>
         </div>
     )
 }
--- src/components/Authenticate/Signup.js
+++ src/components/Authenticate/Signup.js
@@ -9,7 +9,24 @@ export default function Signup(){
     return(
         <div>
             <h1>Signup</h1>
+            <div className="txt_field">
+                <label htmlFor="username">Username: </label>
+                <input type="email" name="" id="" />
+            </div>
+            <div className="txt_field">
+                <label htmlFor="email">Email: </label>
+                <input type="email" name="" id="" />
+            </div>
+            <div className="txt_field">
+                <label htmlFor="password">Password: </label>
+                <input type="email" name="" id="" />
+            </div>
+            <div className="txt_field">
+                <label htmlFor="password">Confirm-Password: </label>
+                <input type="email" name="" id="" />
+            </div>
             <button onClick={OpenLogin}>Signup</button>
+            <button onClick={OpenLogin}>go-to-Login</button>
         </div>
     )
 }
