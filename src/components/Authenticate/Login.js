6312f7f17faed1d071a52fc6109793125e66e990
shuaib shaban <shuaib>
2023-03-30 23:10:46 +0300

    login, signup page setup

--- src/components/Authenticate/Login.js
+++ src/components/Authenticate/Login.js
@@ -1,50 +1,79 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"
 import Eye from "../../images/eye.png";
 import Modal from "react-modal";
 import ForgotPassword from "./ForgotPassword";
 
-Modal.setAppElement("#root"); // this is needed for accessibility
+
 
 export default function Login({ setLoggedIn, setShowLogin }) {
   const [showPassword, setShowPassword] = useState(false);
-  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  const [userLogin, setUserLogin] = useState({
+    username: "",
+    password: ""
+  });
+  const [error, setError] = useState("");
 
-const toggleShowPassword = () => {setShowPassword(!showPassword);};
-const handleForgotPassword = () => {setShowForgotPassword(true);};  // show forgot-password popup
-const handleForgotPasswordClose = () => {setShowForgotPassword(false);}; // hide forgot-password popup
-const OpenSignup = () => {setShowLogin(false);};
+  let takeHome = useNavigate(); 
 
-  const handleLogin = () => {
-    setLoggedIn(true);
-  };
+  const toggleShowPassword = () => setShowPassword(!showPassword);
+
+  // const OpenSignup = () => setShowLogin(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+     fetch('https://book-api-6jbp.onrender.com/users/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify( userLogin )
+    })
+    .then (response => {
+      if (response.ok) {
+        takeHome('/home');
+      }
+      else {
+        setError('Invalid username or password');
+      }
+    })
+  }
 
 
   return (
+    
     <div className="signup-card">
       <h1>Login-page</h1>
-      <div className="txt_field">
-        <label htmlFor="email">Email: </label>
-        <input type="email" placeholder="Email-address" id="" />
-      </div>
-      <div className="txt_field">
-        <label htmlFor="password">Password: </label>
-        <input
-          type={showPassword ? "text" : "password"}
-          placeholder="Password"
-          id="password"
-        />
-        <span className="show-password" onClick={toggleShowPassword}>
-          <img style={{ width: "40px" }} src={Eye} alt="Show Password" />
-        </span>
-      </div>
-      <div className="buttons">
-        <button onClick={handleLogin}>Login</button>
-        <button id="close" onClick={handleForgotPassword}>Forgot Password</button>
-        <button onClick={OpenSignup}>Create-Account</button>
-      </div>
-      <Modal isOpen={showForgotPassword} onRequestClose={handleForgotPasswordClose} >
-        <ForgotPassword onClose={handleForgotPasswordClose} />
-      </Modal>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="txt_field">
+          <label htmlFor="username">username: </label>
+          <input
+            type="text"
+            placeholder="username"
+            id="username"
+            value={userLogin.username}
+            onChange={(e) => setUserLogin({...userLogin, username: e.target.value})}
+          />
+        </div>
+        <div className="txt_field">
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            value={userLogin.password}
+            onChange={(e) => setUserLogin({...userLogin, password: e.target.value})}
+          />
+          <span className="show-password" onClick={toggleShowPassword}>
+            <img style={{ width: "40px" }} src={Eye} alt="Show Password" />
+          </span>
+        </div>
+        <div className="buttons">
+          <button type="submit">Login</button>
+          <button onClick={()=> takeHome('/signup') }>Create-Account</button>
+        </div>
+      </form>
+
     </div>
+    
   );
 }
